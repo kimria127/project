82dbf51ac0226ae8a36da254dbd57921e51c3c7e
kimria127 <gold011127@naver.com>
2023-09-25 18:48:33 +0900

    2주차 개발 시작

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.info, name='info'),
+    path('content/', views.index, name='index'),
+    path('login/', views.login, name='login'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from .models import MainContent
 def info(request):
     return render(request, "pages/company_info.html")
+
+def index(request):
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'pages/content_list.html',context)
+
+def login(request):
+    return render(request, "pages/login.html")
